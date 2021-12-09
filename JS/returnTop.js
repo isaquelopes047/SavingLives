014e64a396c2bfadf566fc9659b5a5291fddc9a8
Yoona Blaska <isaque.lopes047@gmail.com>
2021-12-09 19:42:47 -0300

    add

---
+++ JS/returnTop.js
@@ -0,0 +1,7 @@
+
+
+let buttonReturn = document.querySelector('.up');
+
+buttonReturn.addEventListener("click", function(){
+    window.scrollTo(0, 0);
+});
---
+++ JS/title.js
@@ -0,0 +1,18 @@
+
+var texto = "have you ever thought about changing the word?";
+var result;
+
+var count = 0;
+function digitar() {
+    result = document.getElementById("result");
+    window.setTimeout(function() { inserir(texto[count]) }, 100);
+}
+
+function inserir(letra) {
+    result.innerHTML += letra;
+    count++;
+    if(count < texto.length)
+    window.setTimeout(function() { inserir(texto[count]) }, 100);
+}
+
+window.onload = digitar;
